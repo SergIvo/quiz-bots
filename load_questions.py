6f6b41de7526caa52fb649cfdbed7b005030bec1
Sergey <Sergey@localhost.localdomain>
2023-06-20 12:54:41 +0300

    Parse questions from raw text

--- load_questions.py
+++ load_questions.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from argparse import ArgumentParser
 
@@ -7,6 +8,23 @@ def get_questions_from_file(file_path):
     with open(file_path, 'r', encoding='KOI8-R') as file:
         content = file.read()
     return content
+
+
+def parse_questions_from_text(text):
+    question_blocks = re.findall(r'\bВопрос.+?Ответ:.+?(?=\n\n)', text, flags=re.DOTALL)
+    print('Found: ', len(question_blocks))
+    
+    questions = []
+    for question_block in question_blocks:
+        question = re.search(r'(?<=:\s).+(?=\sОтвет)', question_block, flags=re.DOTALL).group()
+        try:
+            answer = re.search(r'(?<=Ответ:\s).+', question_block, flags=re.DOTALL).group()
+        except:
+            print(question_block)
+            answer = None
+        questions.append({question: answer})
+    
+    return questions
 
 
 if  __name__ == '__main__':
@@ -19,7 +37,9 @@ if  __name__ == '__main__':
     args = parser.parse_args()
     questions_directory = args.questions_directory
 
-    questions_filenames = os.listdir(args.questions_directory)    
-    file_path = os.path.join(questions_directory, questions_filenames[0])
-    file_content = get_questions_from_file(file_path)
-    print(file_content[:20])
+    questions_filenames = os.listdir(args.questions_directory)
+    for filename in questions_filenames:
+        file_path = os.path.join(questions_directory, filename)
+        questions_text = get_questions_from_file(file_path)
+        questions = parse_questions_from_text(questions_text)
+

87f7c54cf0d22c42ad355ffddf778ddf33c5f6b7
Sergey <Sergey@localhost.localdomain>
2023-06-20 18:24:25 +0300

    Send questions via Teelgram

--- tg_bot.py
+++ tg_bot.py
@@ -1,32 +1,46 @@
-from telegram import Update, ForceReply
+import json
+from random import choice
+
+from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from environs import Env
 
 
 def start(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /start is issued."""
     user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Hi {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
+    update.message.reply_text(
+        f'Привет, {user}!'
     )
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+def reply_with_keyboard(update: Update, context: CallbackContext) -> None:
+    keyboard = [['Random button'], ['Назад', 'Новый вопрос']]
+    keyboard_markup = ReplyKeyboardMarkup(keyboard)
+    if update.message.text == 'Новый вопрос':
+        questions = context.bot_data.get('questions')
+        new_question = choice(questions)
+        message_text = new_question['question']
+    else:
+        message_text = 'Random text'
+    update.message.chat.send_message(
+        message_text,
+        reply_markup=keyboard_markup
+    )
 
 
 if __name__ == '__main__':
     env = Env()
     env.read_env()
     tg_api_key = env('TG_API_KEY')
+    with open('questions.json', 'r') as file:
+        questions = json.loads(file.read())
 
     updater = Updater(tg_api_key)
     dispatcher = updater.dispatcher
+    dispatcher.bot_data['questions'] = questions
 
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, reply_with_keyboard))
 
     updater.start_polling()
     updater.idle()

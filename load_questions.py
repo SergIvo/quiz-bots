6731a8e168856e3592d21e43a1a4637da8c3d35d
Sergey <Sergey@localhost.localdomain>
2023-06-20 23:21:23 +0300

    Change the way questions are saved

--- load_questions.py
+++ load_questions.py
@@ -1,5 +1,6 @@
 import os
 import re
+import json
 
 from argparse import ArgumentParser
 
@@ -14,7 +15,7 @@ def parse_questions_from_text(text):
     question_blocks = re.findall(r'\bВопрос.+?Ответ:.+?(?=\n\n)', text, flags=re.DOTALL)
     print('Found: ', len(question_blocks))
     
-    questions = []
+    questions = {}
     for question_block in question_blocks:
         question = re.search(r'(?<=:\s).+(?=\sОтвет)', question_block, flags=re.DOTALL).group()
         try:
@@ -22,9 +23,20 @@ def parse_questions_from_text(text):
         except:
             print(question_block)
             answer = None
-        questions.append({question: answer})
-    
+        questions[question] = answer
     return questions
+
+
+def load_all_questions(questions_directory):
+    questions_filenames = os.listdir(args.questions_directory)
+    all_questions = {}
+    for filename in questions_filenames:
+        file_path = os.path.join(questions_directory, filename)
+        questions_text = get_questions_from_file(file_path)
+        questions = parse_questions_from_text(questions_text)
+        all_questions.update(questions)
+    return all_questions
+
 
 
 if  __name__ == '__main__':
@@ -38,8 +50,7 @@ if  __name__ == '__main__':
     questions_directory = args.questions_directory
 
     questions_filenames = os.listdir(args.questions_directory)
-    for filename in questions_filenames:
-        file_path = os.path.join(questions_directory, filename)
-        questions_text = get_questions_from_file(file_path)
-        questions = parse_questions_from_text(questions_text)
-
+    questions_text = get_questions_from_file(os.path.join(questions_directory, questions_filenames[3]))
+    questions = parse_questions_from_text(questions_text)
+    with open('questions.json', 'w') as file:
+        file.write(json.dumps(questions, indent=4))

b00cb49929fccb0713f64f8b6ea3bce86e177dc9
Sergey <Sergey@localhost.localdomain>
2023-08-02 15:28:44 +0300

    Limit the number of processed files with questions

--- load_questions.py
+++ load_questions.py
@@ -34,10 +34,10 @@ def parse_questions_from_text(text):
     return questions
 
 
-def load_all_questions(questions_directory):
+def load_questions_from_files(questions_directory, limit=None):
     questions_filenames = os.listdir(args.questions_directory)
     all_questions = {}
-    for filename in questions_filenames:
+    for filename in questions_filenames[:limit]:
         file_path = os.path.join(questions_directory, filename)
         questions_text = get_questions_from_file(file_path)
         questions = parse_questions_from_text(questions_text)
@@ -52,9 +52,15 @@ if __name__ == '__main__':
         type=str,
         help='path to directory with questions files'
     )
+    parser.add_argument(
+        'files_limit',
+        type=str,
+        help='Number of questions files to load'
+    )
     args = parser.parse_args()
     questions_directory = args.questions_directory
+    files_limit = int(args.files_limit)
 
-    all_questions = load_all_questions(questions_directory)
-    with open('questions.json', 'w') as file:
-        file.write(json.dumps(all_questions, indent=4))
+    all_questions = load_questions_from_files(questions_directory, files_limit)
+    with open('questions.json', 'w', encoding='utf8') as file:
+        json.dump(all_questions, file, indent=4, ensure_ascii=False)
